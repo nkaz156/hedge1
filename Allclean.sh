e7e8967d11067eaa133dcf579af6d49992196bea
Nathan Kaczka <nkaczka6@gmail.com>
2025-01-24 16:50:04 -0500

    Added OBJ geometry

--- Allclean.sh
+++ Allclean.sh
@@ -3,6 +3,24 @@ cd "${0%/*}" || exit                                # Run from this directory
 . ${WM_PROJECT_DIR:?}/bin/tools/CleanFunctions      # Tutorial clean functions
 #------------------------------------------------------------------------------
 
-cleanCase0
+# Copied most cleanCase functions except the one that deletes .foam files from 
+# $WM_PROJECT_DIR/bin/tools/CleanFunctions
+    cleanTimeDirectories
+    cleanAdiosOutput
+    cleanDynamicCode
+    cleanOptimisation
+    cleanPostProcessing
+
+    cleanFaMesh
+    cleanPolyMesh
+    cleanSnappyFiles
+
+    rm -rf processor*
+    rm -rf TDAC
+    rm -rf probes*
+    rm -rf forces*
+    rm -rf graphs*
+    rm -rf sets
+    rm -rf system/machines
 
 #------------------------------------------------------------------------------
